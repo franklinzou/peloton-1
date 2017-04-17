8329c510f591f4fa23e6b2584434f7bb2daf1d7b
zoushaokun <zoushaokun11@outlook.com>
2017-04-17 17:59:37 -0400

    add start server in main function

--- src/main/peloton/peloton.cpp
+++ src/main/peloton/peloton.cpp
@@ -42,6 +42,8 @@ int main(int argc, char *argv[]) {
     // Launch server
     peloton::wire::LibeventServer libeventserver;
 
+    //
+    libeventserver.StartServer();
     // Teardown
     peloton::PelotonInit::Shutdown();
   }
