80f386331a11777904c7212b6563734eb55fd919
zoushaokun <zoushaokun11@outlook.com>
2017-04-21 16:26:02 -0400

    add po

--- src/wire/libevent_server.cpp
+++ src/wire/libevent_server.cpp
@@ -183,6 +183,7 @@ void LibeventServer::StartServer() {
                                      SSL_FILETYPE_PEM) == 0)
     {
       SSL_CTX_free(ssl_context);
+      ERR_print_errors_fp(stderr);
       throw ConnectionException("Error associating certificate.\n");
     }
 
